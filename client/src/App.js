57bccf5ea5085fa57fd1484c413c3ae5acf3295f
Brave Kim <ygflove@likelion.org>
2021-08-03 05:28:22 +0900

    :tada: first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadForm from './components/UploadForm';
+
+const App = () => {
+  return (
+    <div>
+      <h2>이미지 업로드</h2>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/UploadForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const UploadForm = _ => {
+  const [file, setFile] = useState(null);
+  const [filename, setFileName] = useState("이미지 파일을 업로드 해주세요.");
+  const imageSelectHandler = (e) => {
+    const imageFile = e.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" }
+      });
+      console.log({ res });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="image">{filename}</label>
+      <input id="image" type="file" onChange={imageSelectHandler} />
+      <button type="submit">제출</button>
+    </form>
+  )
+}
+
+export default UploadForm;
