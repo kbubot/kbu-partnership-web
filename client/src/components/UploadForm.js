18cf40027c9c6848056759e22452ae2acd16ed7f
Brave Kim <ygflove@likelion.org>
2021-08-14 18:24:03 +0900

    :zap: multiple image upload and preview

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -8,26 +8,38 @@ import { ImageContext } from '../context/ImageContext';
 
 const UploadForm = _ => {
   const { images, setImages, myImages, setMyImages } = useContext(ImageContext);
-  const defaultFileName = "이미지 파일을 업로드 해주세요.";
-  const [file, setFile] = useState(null);
-  const [imgSrc, setImgSrc] = useState(null);
-  const [filename, setFileName] = useState(defaultFileName);
+  const [files, setFiles] = useState(null);
+
+  const [previews, setPreviews] = useState([]);
+
   const [percent, setPercent] = useState(0);
   const [click, setClick] = useState(true);
   const [isPublic, setIsPublic] = useState(true);
 
-  const imageSelectHandler = (e) => {
-    const imageFile = e.target.files[0];
-    setFile(imageFile);
+  const imageSelectHandler = async (e) => {
     /**
      * TODO: 이미지가 선택되지 않았는데 제출을 눌렀을 경우 에러 처리해야함
      */
-    setFileName(imageFile.name);
+    const imageFiles = e.target.files;
+    setFiles(imageFiles);
 
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(imageFile);
-    fileReader.onload = e => setImgSrc(e.target.result);
-    setImgSrc();
+    const imagePreviews = await Promise.all(
+      [...imageFiles].map(imageFile => {
+        return new Promise((resolve, reject) => {
+          try {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(imageFile);
+            fileReader.onload = e => resolve({
+              imgSrc: e.target.result,
+              fileName: imageFile.name
+            });
+          } catch (err) {
+            reject(err);
+          }
+        });
+      })
+    );
+    setPreviews(imagePreviews);
   };
   const onSubmit = async e => {
     /**
@@ -35,7 +47,8 @@ const UploadForm = _ => {
      */
     e.preventDefault();
     const formData = new FormData();
-    formData.append("image", file);
+    for (let file of files)
+      formData.append("image", file);
     formData.append("public", isPublic);
     try {
       const res = await axios.post("/images", formData, {
@@ -45,36 +58,51 @@ const UploadForm = _ => {
         },
       });
       if (isPublic)
-        setImages([...images, res.data]);
+        setImages([...images, ...res.data]);
       else
-        setMyImages([...myImages, res.data]);
+        setMyImages([...myImages, ...res.data]);
 
       toast.success("이미지 업로드 성공!");
       setClick(false);
       setTimeout(() => {
         setPercent(0);
-        setFileName(defaultFileName);
-        setImgSrc(null);
+        setPreviews([]);
       }, 3000);
     } catch (err) {
       toast.error(err.response.data.message);
       setPercent(0);
-      setFileName(defaultFileName);
-      setImgSrc(null);
+      setPreviews([]);
       console.error(err);
     }
   }
+  const previewImages = previews.map((preview, index) => (
+    <img
+      key={index}
+      style={{ width: 200, height: 200, objectFit: 'cover' }}
+      src={preview.imgSrc}
+      alt=""
+      className={`image-preview ${preview.imgSrc && "image-preview-show"}`}
+    />
+  ));
+  const fileName = previews.length === 0 ?
+    "이미지 파일을 업로드 해주세요." :
+    previews.reduce((previous, current) => previous + ` ${current.fileName}`, "");
   return (
     <form className="file-sender" onSubmit={onSubmit}>
-      <img
-        className={`image-preview ${imgSrc && "image-preview-show"}`}
-        src={imgSrc}
-        alt=""
-      />
+      <div style={{
+        display: "flex",
+        flexWrap: "wrap"
+      }}>
+        {previewImages}
+      </div>
       <ProgressBar percent={percent} />
       <div className="file-dropper">
-        {filename}
-        <input id="image" type="file" accept="image/*"
+        {fileName}
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          multiple
           onChange={imageSelectHandler}
         />
       </div>
@@ -82,7 +110,7 @@ const UploadForm = _ => {
         type="checkbox"
         id="public-check"
         value={isPublic}
-        onChange={e => { setIsPublic(!isPublic) }}
+        onChange={_ => { setIsPublic(!isPublic) }}
       />
       <label htmlFor="public-check">비공개</label>
       <button type="submit" disabled={!click}>제출</button>
