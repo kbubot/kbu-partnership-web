b315b474eb55f1fad53b43968f4e3067d5df39eb
Brave Kim <ygflove@likelion.org>
2021-09-16 23:03:19 +0900

    :white_check_mark: Add searchbar UI
    Need api

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import axios from "axios";
+
+import { Paper, InputBase, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+
+import { ImageContext } from '../context/ImageContext';
+
+const useStyles = makeStyles((theme) => ({
+  headline: {
+    textAlign: "center",
+  },
+  logo: {
+    width: 140,
+    margin: "auto",
+    display: "block",
+  },
+  paper: {
+    display: "flex",
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+}));
+
+const SearchBar = () => {
+  const classes = useStyles();
+  // const { setImages, setMyImages } = useContext(ImageContext);
+
+  const onChange = async e => {
+
+  };
+  const onSubmit = async e => {
+
+  };
+
+  return (
+    <div>
+      <Paper component="form" className={classes.paper} onSubmit={onSubmit}>
+        <InputBase className={classes.input} onChange={onChange} />
+        <IconButton type="submit">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </div>
+  );
+};
+
+export default SearchBar;
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+
+import { AuthContext } from '../context/AuthContext';
+
 import UploadForm from '../components/UploadForm';
 import ImageList from '../components/ImageList';
-import { AuthContext } from '../context/AuthContext';
+import SearchBar from '../components/SearchBar';
 
 const MainPage = () => {
   const [me] = useContext(AuthContext);
 
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>이미지 업로드</h2>
+      <h2 style={{ textAlign: "center" }}>새 제휴업체 등록</h2>
       {me && <UploadForm />}
+      <SearchBar />
       <ImageList />
     </div>
   );
