d1bd6f0bf6f0ab7bb25f5d89ea8663fb1d0826c9
Brave Kim <ygflove@likelion.org>
2021-09-22 20:09:49 +0900

    :white_check_mark: Add 'New partnership register' feature
    Just UI

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,21 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 
-import { AuthContext } from '../context/AuthContext';
+import { Divider } from '@material-ui/core';
+import { ToggleButton } from '@material-ui/lab'
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
-import UploadForm from '../components/UploadForm';
 import ImageList from '../components/ImageList';
 import SearchBar from '../components/SearchBar';
+import InfoForm from '../components/InfoForm';
+import UploadForm from '../components/UploadForm';
 
 const MainPage = () => {
-  const [me] = useContext(AuthContext);
-
+  const [selected, setSelected] = useState(false);
   return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>새 제휴업체 등록</h2>
-      {me && <UploadForm />}
-      <SearchBar />
-      <ImageList />
-    </div>
+    <>
+      <div
+        style={{
+          width: "100%",
+          clear: "both",
+          overflow: "auto"
+        }}
+      >
+        <h2
+          style={{
+            display: "inline-block",
+            float: "left"
+          }}
+        >새 제휴업체 등록</h2>
+        <ToggleButton
+          style={{
+            float: "right",
+            verticalAlign: "middle",
+            position: "relative",
+            top: "50%"
+          }}
+          value="check"
+          selected={selected}
+          onChange={_ => setSelected(!selected)}
+        >
+          {selected ? <HighlightOffIcon /> : <ControlPointIcon />}
+        </ToggleButton>
+      </div>
+      {selected
+        ? <div style={{ maxWidth: 350, margin: 'auto' }}>
+          <UploadForm />
+          <Divider style={{ margin: "20px 0" }} />
+          <InfoForm />
+        </div>
+        : <>
+          <SearchBar />
+          <ImageList />
+        </>
+      }
+    </>
   );
 };
 
