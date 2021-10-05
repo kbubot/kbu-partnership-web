3878bfca1c285b49e7f37643821b645e723c56bf
Brave Kim <ygflove@likelion.org>
2021-10-06 03:00:46 +0900

    :rocket: Deploy build client-app on the static server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require("express");
 const mongoose = require('mongoose');
+const path = require("path");
+
 const { imageRouter } = require("./routes/imageRouter");
 const { userRouter } = require("./routes/userRouter");
 const { partnerRouter } = require("./routes/partnerRouter");
@@ -18,11 +20,16 @@ mongoose
   .then(_ => {
     console.log("MongoDB Connected.")
     app.use("/uploads", express.static("uploads"));
+    app.use(express.static(path.join(__dirname, "../client/build")));
     app.use(express.json());
     app.use(authenticate);
     app.use("/images", imageRouter);
     app.use("/users", userRouter);
     app.use("/partner", partnerRouter);
+
+    app.get("/", (_, res) => {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
     app.listen(PORT, () => console.log('Express server listening on PORT ' + PORT));
   })
   .catch(err => console.log(err));
